04a781b2dc3223718ec197291d83ded5f4023a01
samuel0325f <samuelantoniomartinsbalbino@gmail.com>
2025-11-21 10:53:39 -0300

    Alterando o valor de PI 3.16

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function medidas(raio){
-    let pi = 3.14 ;
+    let pi = 3.16 ;
     let area = raio * raio * pi;
     let perimetro = 2 * raio * pi;
     alert (`O perimetro é de ${perimetro}, e a área é de ${area}`);
