f2389bc14186f37fbf62c052c4c9ac78c39bea83
Samuel Antonio <61150294+samuel0325f@users.noreply.github.com>
2025-11-25 14:01:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function medidas(raio){
-    let pi = 3.18 ;
+    let pi = 3.17 ;
     let area = raio * raio * pi;
     let perimetro = 2 * raio * pi;
     alert (`O perimetro é de ${perimetro}, e a área é de ${area}`);
